caaab00ace8536cc619b4b97ed76c35f9c0aae50
finnmattis <95555850+finnmattis@users.noreply.github.com>
2024-06-24 10:34:37 -0700

    a

--- train.py
+++ train.py
@@ -159,7 +159,7 @@ val_loader = DataLoaderLite(process_rank=ddp_rank, num_processes=ddp_world_size,
 # create model
 model = GPT(GPTConfig(vocab_size=50304)) # "nice" number
 model.to(device)
-use_compile = True # torch.compile interferes with HellaSwag eval and Generation. TODO fix
+use_compile = True
 if use_compile:
     model = torch.compile(model)
 if ddp:
@@ -207,7 +207,6 @@ def eval_val_loss():
             f.write(f"{step} val {val_loss_accum.item():.4f}\n")
         if (step % CHECKPOINT_INTERVAL == 0 or last_step):
             print("checkpoint time!!!")
-            # optionally write model checkpoints
             checkpoint_path = os.path.join(log_dir, f"model_{step:05d}.pt")
             checkpoint = {
                 'model': raw_model.state_dict(),
@@ -215,78 +214,7 @@ def eval_val_loss():
                 'step': step,
                 'val_loss': val_loss_accum.item()
             }
-            # you might also want to add optimizer.state_dict() and
-            # rng seeds etc., if you wanted to more exactly resume training
             torch.save(checkpoint, checkpoint_path)
-
-# Eval Hella Swag Benchmark
-def eval_hella_swag():
-    num_correct_norm = 0
-    num_total = 0
-    for i, example in enumerate(iterate_examples("val")):
-        # only process examples where i % ddp_world_size == ddp_rank
-        if i % ddp_world_size != ddp_rank:
-            continue
-        # render the example into tokens and labels
-        _, tokens, mask, label = render_example(example)
-        tokens = tokens.to(device)
-        mask = mask.to(device)
-        # get the logits
-        with torch.no_grad():
-            with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
-                logits, loss = model(tokens)
-            pred_norm = get_most_likely_row(tokens, mask, logits)
-        num_total += 1
-        num_correct_norm += int(pred_norm == label)
-    # reduce the stats across all processes
-    if ddp:
-        num_total = torch.tensor(num_total, dtype=torch.long, device=device)
-        num_correct_norm = torch.tensor(num_correct_norm, dtype=torch.long, device=device)
-        dist.all_reduce(num_total, op=dist.ReduceOp.SUM)
-        dist.all_reduce(num_correct_norm, op=dist.ReduceOp.SUM)
-        num_total = num_total.item()
-        num_correct_norm = num_correct_norm.item()
-    acc_norm = num_correct_norm / num_total
-    if master_process:
-        print(f"HellaSwag accuracy: {num_correct_norm}/{num_total}={acc_norm:.4f}")
-        with open(log_file, "a") as f:
-            f.write(f"{step} hella {acc_norm:.4f}\n")
-
-# Samples to see what model output looks like atm
-def sample():
-    model.eval()
-    num_return_sequences = 4
-    max_length = 32
-    tokens = enc.encode("Hello, I'm a language model,")
-    tokens = torch.tensor(tokens, dtype=torch.long)
-    tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
-    xgen = tokens.to(device)
-    sample_rng = torch.Generator(device=device)
-    sample_rng.manual_seed(SEED+1 + ddp_rank)
-    while xgen.size(1) < max_length:
-        # forward the model to get the logits
-        with torch.no_grad():
-            with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
-                logits, loss = model(xgen) # (B, T, vocab_size)
-            # take the logits at the last position
-            logits = logits[:, -1, :] # (B, vocab_size)
-            # get the probabilities
-            probs = F.softmax(logits, dim=-1)
-            # do top-k sampling of 50 (huggingface pipeline default)
-            # topk_probs here becomes (5, 50), topk_indices is (5, 50)
-            topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
-            # select a token from the top-k probabilities
-            # note: multinomial does not demand the input to sum to 1
-            ix = torch.multinomial(topk_probs, 1, generator=sample_rng) # (B, 1)
-            # gather the corresponding indices
-            xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
-            # append to the sequence
-            xgen = torch.cat((xgen, xcol), dim=1)
-    # print the generated text
-    for i in range(num_return_sequences):
-        tokens = xgen[i, :max_length].tolist()
-        decoded = enc.decode(tokens)
-        print(f"rank {ddp_rank} sample {i}: {decoded}")
 
 ####################################################################################################
 #                                        Train                                                     #
@@ -301,14 +229,6 @@ for step in range(MAX_STEPS):
     # once in a while evaluate our validation loss
     if step % EVAL_INTERVAL == 0 or last_step:
         eval_val_loss()
-
-    # once in a while evaluate hellaswag
-    # if (step % 250 == 0 or last_step) and (not use_compile):
-    #     eval_hella_swag()
-
-    # once in a while generate from the model (except step 0, which is noise)
-    # if ((step > 0 and step % 250 == 0) or last_step) and (not use_compile):
-    #     sample()
 
     # optim step
     model.train()
